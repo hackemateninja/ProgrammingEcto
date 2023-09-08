386896d6472691733cbfc5523e66a5b971533e5f
Herman <nitrowitty@gmail.com>
2023-09-08 00:32:39 -0600

    schema asociations

--- practice/schema_module.ex
+++ practice/schema_module.ex
@@ -1,6 +1,6 @@
 defmodule Practice.SchemaModule do
   import Ecto.Query
-  alias MusicDB.{Repo, Track, Artist}
+  alias MusicDB.{Repo, Track, Artist, Album}
 
   def data_schemaless() do
     artist = "1"
@@ -33,13 +33,31 @@ defmodule Practice.SchemaModule do
   end
 
   def deleting() do
-    track = Repo.get_by(Track, title: "The Moontrane")
-    Repo.delete(Track, title: "The Moontrane")
+    #track = Repo.get_by(Track, title: "The Moontrane")
+    #Repo.delete(track)
     #Repo.delete_all("tracks")
+    q = from MusicDB.Artist
+    Repo.all q
+  end
 
+  def getting_associated() do
+    albums =
+      Album
+      |>Repo.all
+      |>Repo.preload(:tracks)
+
+    albums
+
+    q = from a in Album,
+      join: t in assoc(a, :tracks),
+      where: t.title == "Freddie Freeloader",
+      preload: [tracks: t]
+
+    Repo.all q
   end
 end
 #IO.inspect(Practice.SchemaModule.data_schemaless())
 #IO.inspect(Practice.SchemaModule.query_with_schema())
 #IO.inspect(Practice.SchemaModule.inserting())
-IO.inspect(Practice.SchemaModule.deleting())
+#IO.inspect(Practice.SchemaModule.deleting())
+IO.inspect(Practice.SchemaModule.getting_associated)
